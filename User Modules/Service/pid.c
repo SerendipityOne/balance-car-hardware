829c3a332fd06de9172d5339eee35be4d700ffcc
Serendipity <1402485721@qq.com>
2025-09-09 19:56:01 +0800

    编写PID算法

--- User Modules/Service/pid.c
+++ User Modules/Service/pid.c
@@ -1,3 +1,41 @@
 #include "pid.h"
 
+VerticalParams vertical_params;  // 直立环控制参数
+VelocityParams velocity_params;  // 速度环控制参数
+TurnParams turn_params;          // 转向环控制参数
+uint8_t stop;                    // 停止标志位
 
+// 直立环PD控制器
+// 输入：期望角度、真实角度、角速度
+// 一个典型的PD控制器，用于保持系统直立
+int vertical_pid(VerticalPID pid) {
+	// kp*(角度误差) + kd*角速度
+  return vertical_params.kp * (pid.angle - pid.med) + vertical_params.kd * pid.gyro_y;
+}
+
+// 速度环PI控制器
+// 输入:期望速度、左编码器、右编码器
+int vertical_speed_pid(VerticalSpeedPID pid) {
+  static int err_lowout_last, encoder_s;
+  static float a = 0.7;
+  // 计算误差
+  int err, err_lowout;
+  err = pid.encoder_data.encoder_left + pid.encoder_data.encoder_right - pid.target;
+  // 低通滤波
+  err_lowout = (1 - a) * err + err_lowout_last;
+  err_lowout_last = err_lowout;
+  // 计算积分
+  encoder_s += err_lowout;
+  // 积分限幅（-20000~20000）
+  encoder_s = encoder_s > 20000 ? 20000 : (encoder_s < -20000 ? -20000 : encoder_s);
+  if (stop == 1) encoder_s = 0, stop = 0;
+	// kp*滤波后误差 + ki*积分项
+  return velocity_params.kp * err_lowout + velocity_params.ki * encoder_s;
+}
+
+//转向环PD控制器
+//输入：角速度、角度值
+int turn(TurnPID pid) {
+	// kp*目标角度 + kd*角速度
+  return turn_params.kp * pid.target + turn_params.kd * pid.gyro_z;
+}
--- User Modules/Service/pid.h
+++ User Modules/Service/pid.h
@@ -1,6 +1,39 @@
 #ifndef __PID_H__
 #define __PID_H__
 
+#include "encoder.h"
 #include "stm32f1xx_hal.h"
+
+// PID 控制器参数结构体
+typedef struct VerticalPID {
+  float med;     // 期望角度
+  float angle;   // 真实角度
+  float gyro_y;  // 角速度
+} VerticalPID;
+
+typedef struct VerticalParams {
+  float kp;  // 比例增益
+  float kd;  // 微分增益
+} VerticalParams;
+
+typedef struct VerticalSpeedPID {
+  int target;
+  Encoder encoder_data;
+} VerticalSpeedPID;
+
+typedef struct VelocityParams {
+  float kp;  // 比例增益
+  float ki;  // 积分增益
+} VelocityParams;
+
+typedef struct TurnPID {
+  float gyro_z;  // 角速度
+  int target;    // 目标角度
+} TurnPID;
+
+typedef struct TurnParams {
+  float kp;  // 比例增益
+  float kd;  // 微分增益
+}TurnParams;
 
 #endif  // !__PID_H__
